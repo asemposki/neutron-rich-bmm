ad61673625b6c23d97b4578d41e03912bf569343
Alexandra C. Semposki <65143779+asemposki@users.noreply.github.com>
2025-10-07 13:36:24 -0400

    adding version init file

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from importlib.metadata import version
+
+__version__ = version("neutron-rich-bmm")
+__author__ = "Alexandra C. Semposki, C. Drischler, R. J. Furnstahl, D. R. Phillips"
+__credits__ = "Ohio University, Facility for Rare Isotope Beams, The Ohio State University"
