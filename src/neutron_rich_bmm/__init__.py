6ddbbaec93c60307b0f1841d3ba1d6ec1eb06432
Alexandra C. Semposki <65143779+asemposki@users.noreply.github.com>
2025-10-15 20:21:27 -0400

    fix the init file
    Removed fallback for version retrieval in local builds.

--- src/neutron_rich_bmm/__init__.py
+++ src/neutron_rich_bmm/__init__.py
@@ -1,10 +1,5 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import version
 
-try:
-    __version__ = version("neutron-rich-bmm")
-except PackageNotFoundError:
-    # Fallback for local builds or GitHub Pages environments
-    __version__ = "0.0.0"
-
+__version__ = version("neutron-rich-bmm")
 __author__ = "Alexandra C. Semposki, C. Drischler, R. J. Furnstahl, D. R. Phillips"
 __credits__ = "Ohio University, Facility for Rare Isotope Beams, The Ohio State University"
